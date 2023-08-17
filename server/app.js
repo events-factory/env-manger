339e3cd96271af64ac756ba415d8a6a1a8a99344
Aime Malaika <aimemalaika1995@gmail.com>
2023-08-17 16:31:25 +0200

    Fix bycrypt

--- server/app.js
+++ server/app.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
 
 app.use(express.static(path.join(__dirname, '../public'), { 'extensions': ['html', 'htm', 'css'] }));
 
