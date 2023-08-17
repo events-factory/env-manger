77a6ae7e421bee6b10cc3a3e68cf9cfc6e40212a
Aime Malaika <aimemalaika1995@gmail.com>
2023-08-17 16:38:04 +0200

    Fix bycrypt

--- server/project.js
+++ server/project.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs').promises;
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
